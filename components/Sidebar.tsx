b1759a2b14c71cd8518a8ec1e5a2b3ab9823476c
Kshitij-1008 <kshitijjha1008@gmail.com>
2025-08-05 00:09:16 +0530

    Added Breadcrumb headers

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,6 +2,10 @@
 
 import { Coins, HomeIcon, Layers2Icon, ShieldCheckIcon } from "lucide-react";
 import React from "react";
+import Logo from "./Logo";
+import Link from "next/link";
+import { buttonVariants } from "./ui/button";
+import { usePathname } from "next/navigation";
 
 const routes = [
     {
@@ -27,11 +31,33 @@ const routes = [
 ];
 
 function DesktopSidebar() {
+
+    const pathname = usePathname();
+    const activeRoute = routes.find(
+        route => route.href.length > 0 && 
+        pathname.includes(route.href)) || routes[0];
+
     return (
-        <div className="hidden relative md:block min-w-[280px] h-screen 
-        overflow-hidden w-full bg-primary/5 dark:bg-secondary/30 
-        dark:text-foreground text-muted-foreground border-r-2 border-separate">
-            DesktopSidebar
+        <div className="hidden relative md:block min-w-[280px] 
+        max-w-[280px] h-screen overflow-hidden w-full bg-primary/5 dark:bg-secondary/30 
+        dark:text-foreground text-muted-foreground border-r-2 border-separate ">
+            <div className="flex items-center justify-center gap-2 border-b-[1px] border-separate p-4 ">
+                <Logo />
+            </div>
+            <div className="p-2">TODO Credits</div>
+            <div className="flex flex-col pad-2">
+                {routes.map(route => (
+                    <Link key={route.href} href={route.href}
+                    className={
+                        buttonVariants({
+                            variant: activeRoute.href === route.href ?"sidebarActiveItem" :"sidebarItem",
+                        })
+                    }>
+                        <route.icon size={20} />
+                        {route.label}
+                    </Link>
+                ))}
+            </div>
         </div>
     )
 }
