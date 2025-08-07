b26c53c99f1f08fad8e50eb32f821c14e0f3b971
Kshitij-1008 <kshitijjha1008@gmail.com>
2025-08-08 02:16:58 +0530

    started node card

---
+++ app/workflow/_components/nodes/NodeCard.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+
+ 
+
+function NodeCard({ children, nodeId }: { nodeId: string, children: React.ReactNode }) {
+    return (
+        <div>{children}</div>
+    );
+}
+
+export default NodeCard;
