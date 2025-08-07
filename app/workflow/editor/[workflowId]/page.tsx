9e5446c0b0362364229eb7b4d79f17d06298ed71
Kshitij-1008 <kshitijjha1008@gmail.com>
2025-08-08 01:20:21 +0530

    workflow/editor setup completed

--- app/workflow/editor/[workflowId]/page.tsx
+++ app/workflow/editor/[workflowId]/page.tsx
@@ -2,6 +2,7 @@ import { waitFor } from "@/lib/helper/waitFor";
 import prisma from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
 import React from "react";
+import Editor from "../../_components/Editor";
 
 async function page({ params }: { params : {workflowId: string} }) {
 
@@ -10,8 +11,6 @@ async function page({ params }: { params : {workflowId: string} }) {
     if (!userId) {
         return <div>Unauthenticated</div>
     };
-
-    await waitFor(5000);
     
     const workflow = await prisma.workflow.findUnique({
         where: {
@@ -25,9 +24,7 @@ async function page({ params }: { params : {workflowId: string} }) {
     }
 
     return (
-        <pre>
-            {JSON.stringify(workflow, null, 4)}
-        </pre>
+        <Editor workflow={workflow} />
     );
 }
 
